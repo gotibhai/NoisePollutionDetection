744f0d736af348950e5c2011707a51ecb57e4628
Selena Latchman <selena.latchman@rangle.io>
2017-11-19 00:13:58 -0500

    fixed loading of data before use

--- noise-heatmap-app/src/ui/components/heatmapArea.js
+++ noise-heatmap-app/src/ui/components/heatmapArea.js
@@ -1,5 +1,4 @@
 /*global google*/
-// smart component (container)
 
 import React from 'react';
 import { connect } from 'react-redux';
@@ -7,7 +6,7 @@ import $ from 'jquery';
 import firebase from 'firebase'
 var GoogleMapsLoader = require('google-maps'); // only for common js environments 
 
-
+// smart component
 export class HeatmapArea extends React.Component {
   constructor(props) {
     super(props);
@@ -43,13 +42,6 @@ export class HeatmapArea extends React.Component {
     return {
       center: hackWestern,
       zoom: 20,
-      // styles: [{
-      //   featureType: 'poi',
-      //   stylers: [{ visibility: 'off' }]  // Turn off points of interest.
-      // }, {
-      //   featureType: 'transit.station',
-      //   stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
-      // }],
       disableDoubleClickZoom: true
     };
   }
@@ -62,8 +54,6 @@ export class HeatmapArea extends React.Component {
     heatmap.setMap(this.map);
   }
 
-
-
   loadData() {
     // connect to firebase
     var config = {
@@ -75,8 +65,31 @@ export class HeatmapArea extends React.Component {
       messagingSenderId: "1047190133742"
     };
     firebase.initializeApp(config);
+    const compRef = this;
+    
+    // get point data and parse it
+    var starCountRef = firebase.database().ref('EventObj');
+    starCountRef.on('value', function(result) {
+      console.log("db call:", result.val());
+      compRef.dataPoints = [];
+      var heatPoints = [];
+      if (result.val()) {
+        // database map to info object array
+        compRef.dataPoints = Object.values(result.val()).map((row) => {
+          return {location: {lat: row.Location.latitude, lng: row.Location.longitude}, amplitude: row.amplitude, time: row.time, type: row.type}
+        });
+        // info object array map to heatmap array
+        heatPoints = compRef.dataPoints.map((item) => {
+          return {location: new google.maps.LatLng(item.location.lat, item.location.lng), weight: item.amplitude}
+        });
+      }
+      compRef.applyHeadMap(heatPoints);
+      compRef.onDataLoaded();
+    });
+  };
 
-    // create infowindow to display noise type
+  onDataLoaded() {
+    // create infowindow to display noise type    
     var infowindow =  new google.maps.InfoWindow({
       content: "TYPE",
     });
@@ -100,42 +113,23 @@ export class HeatmapArea extends React.Component {
 
       // update infowindow
       console.log('closest noise:', closestNoise)
-      if (closestNoise.type != '') {
+      // Note following calculation should scale with zoom (feature)
+      if (closestNoise.type != '' && closestNoise.dist < 0.0025) {
+        if (closestNoise.type === 'Couldn\'t catch it') {
+          closestNoise.type = 'Unable to classify';
+        }
         infowindow.setContent(closestNoise.type);      
         infowindow.setPosition(closestNoise.loc);
         infowindow.open(compRef.map);
       }
-    });
-    
-    // get point data and parse it
-    var starCountRef = firebase.database().ref('EventObj');
-    starCountRef.on('value', function(result) {
-      console.log("db call:", result.val());
-      compRef.dataPoints = [];
-      var heatPoints = [];
-      if (result.val()) {
-        // database map to info object array
-        compRef.dataPoints = Object.values(result.val()).map((row) => {
-          return {location: {lat: row.Location.latitude, lng: row.Location.longitude}, amplitude: row.amplitude, time: row.time, type: row.type}
-        });
-        // info object array map to heatmap array
-        heatPoints = compRef.dataPoints.map((item) => {
-          return {location: new google.maps.LatLng(item.location.lat, item.location.lng), weight: item.amplitude}
-        });
+      else {
+        infowindow.close();
       }
-      compRef.applyHeadMap(heatPoints);
     });
-  };
+  }
 };
 
-function loadJS(src) {
-  var ref = window.document.getElementsByTagName("script")[0];
-  var script = window.document.createElement("script");
-  script.src = src;
-  script.async = true;
-  ref.parentNode.insertBefore(script, ref);
-};
-
+// Calculates distance between two map coords
 function calcCrow(click, point) 
 {
   var lat1 = click.lat;
